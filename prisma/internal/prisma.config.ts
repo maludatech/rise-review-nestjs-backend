e24b5a07a6575b8928f4ac4431c722fae4d922bd
maludatech <nwitevictor7@gmail.com>
2026-05-23 01:29:42 +0100

    update: a few changes on the prisma.config.ts

--- prisma/internal/prisma.config.ts
+++ prisma/internal/prisma.config.ts
@@ -2,13 +2,13 @@ import 'dotenv/config';
 import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
-  schema: 'prisma/internal.schema.prisma',
+  schema: './internal.schema.prisma',
 
   datasource: {
     url: process.env.INTERNAL_DATABASE_URL,
   },
 
   migrations: {
-    path: 'prisma/migrations/internal',
+    path: './prisma/migrations/internal',
   },
 });
--- prisma/rise-review/prisma.config.ts
+++ prisma/rise-review/prisma.config.ts
@@ -2,13 +2,13 @@ import 'dotenv/config';
 import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
-  schema: 'prisma/rise/rise-review.schema.prisma',
+  schema: './rise-review.schema.prisma',
 
   datasource: {
     url: process.env.RISE_REVIEW_DATABASE_URL,
   },
 
   migrations: {
-    path: 'prisma/rise/migrations',
+    path: './prisma/migrations/rise-review',
   },
 });
